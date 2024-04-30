68792dddace09d1c97271e73b62c690447eef7d8
W4yce <134702182+W4yce@users.noreply.github.com>
2024-04-30 16:06:10 +0300

    Add files via upload
    Задания ДЗ первого урока курса "Автоматизация тестирования на Python"

---
+++ lesson_1_task_7.py
@@ -0,0 +1,35 @@
+def get_1():
+    return '1'
+
+def get_2():
+    return '2'
+
+def get_3():
+    return '3'
+
+def get_4():
+    return '4'
+
+def get_5():
+    return '5'
+
+def get_6():
+    return '6'
+
+def get_7():
+    return '7'
+
+def get_8():
+    return '8'
+
+def get_9():
+    return '9'
+
+def get_0():
+    return '0'
+
+result = ''.join([get_8(), get_8(), get_0(), get_0(), get_5(), get_5(), get_5(), get_3(), get_5(), get_3(), get_5()])
+print(result)
+
+
+
---
+++ lesson_1_task_8.py
@@ -0,0 +1,6 @@
+def get_number(num):
+    return str(num)
+result = "".join([get_number(8), get_number(8), get_number(0), get_number(0), get_number(5), get_number(5), get_number(5), get_number(3), get_number(5), get_number(3), get_number(5)])
+print(result)
+
+    
